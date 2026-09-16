33c76178b89bc4edefc8cdcb54e37a81c4117c62
KumoXD <8d77y49hrd@privaterelay.appleid.com>
2026-09-16 11:26:41 +0930

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='shiftmate-v11-network-first';
+const C='shiftmate-v12-all-weekly-shifts';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
